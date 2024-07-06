7140a00e553f14cb3d866de64f408192b5700ac0
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2024-07-06 13:38:36 +0530

    infinite canvas initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-
-import './App.css'
+import InfiniteCanvas from "ef-infinite-canvas";
+import { useEffect } from "react";
 
 function App() {
-  
+  useEffect(() => {
+    const canvasElement = document.getElementById("canvas");
+    if (canvasElement instanceof HTMLCanvasElement) {
+      const infCanvas = new InfiniteCanvas(canvasElement);
+
+      // get the CanvasRenderingContext2D
+      const ctx = infCanvas.getContext("2d");
+
+      ctx.fillStyle = "#f00";
+      ctx.lineWidth = 4;
+      ctx.beginPath();
+      // Using a large but finite width and height for the rectangle
+      ctx.rect(30, 30, 1000, 30);
+      ctx.fill();
+      ctx.stroke();
+    }
+  }, []);
 
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  )
+   
+      <canvas id="canvas"  width={window.innerWidth} height={window.innerHeight}></canvas>
+   
+  );
 }
 
-export default App
+export default App;
